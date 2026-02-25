bbfa81e1916dfba1306b539b4260aa413e6957bc
Danielle D. <dani.dixonpro@gmail.com>
2026-02-24 22:50:14 -0500

    Addition of family groups function ex.2 to app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ pio.renderers.default = "browser"
 
 from apputil import *
 
+"""Survival_demographics creates a new column, 'Age Group', that creates age groupings 
+for the survivors from the dataset."""
 # Load Titanic dataset
 df = pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/main/data/titanic.csv')
 bins = [0, 12, 19, 59, 110]
@@ -61,6 +63,34 @@ def visualize_demographic():
 
 fig1 = visualize_demographic()
 st.plotly_chart(fig1, use_container_width=True)
+
+"""Family_Groups  creates a new column called 'Family_Size' that calculates 
+the family size of the passengers from the data set."""
+df_family= pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/main/data/titanic.csv')
+
+def family_groups():
+    df_family['Family_Size'] = df_family['SibSp'] + df_family['Parch'] + 1
+    #group by family size and passenger class
+    # for ea group calculate
+    # n_passengers - total num of passengers
+    # avg_fare -average ticket fare 
+    #min_fare, max_fare -min/max ticket faires
+    grouped = (
+        df_family.groupby(["Family_Size", "Pclass"])
+          .agg(
+              n_passengers=("PassengerId", "count"),
+              avg_fare=("Fare", "mean"),
+              min_fare=("Fare", "min"),
+              max_fare=("Fare", "max")
+          )
+          .reset_index()
+          .sort_values(["Pclass", "Family_Size"])
+    )
+
+
+    return grouped
+
+family_groups()
 
 st.write(
 '''
